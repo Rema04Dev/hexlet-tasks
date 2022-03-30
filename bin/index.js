2b83bc1eb9e7405a7faf9332453722ccad6646cb
Rema04Dev <romansmerdyakov@gmail.com>
2022-03-30 21:34:15 +0300

    remove bin directory

--- bin/index.js
+++
@@ -1,9 +0,0 @@
-import dnaToRna from "../introduction/dnaToRna.js";
-
-const run = () => {
-    console.log(dnaToRna('ACGTGGTCTTAA'))
-    console.log(dnaToRna(''))
-    console.log(dnaToRna('ACGNGTA'))
-}
-
-run();
