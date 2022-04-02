62ef4c303023b5c5c1cadda17ad996076d838039
Rema04Dev <romansmerdyakov@gmail.com>
2022-04-02 12:37:33 +0300

    add fizzBuzz.js

--- introduction/dnaToRna.js
+++ introduction/dnaToRna.js
@@ -1,16 +1,16 @@
-const conversionList = 
-    { G: 'C', C: 'G', T: 'A', A: 'U', };
+const conversionList = {
+  G: 'C', C: 'G', T: 'A', A: 'U',
+};
 
 export default (dna) => {
-    if (dna === '') return '';
-    const dnaArr = dna.split('');
-    let unknown = null;
-    const rnaArr = dnaArr.map(el => {
-        if (conversionList[el] === undefined) unknown = true;
+  if (dna === '') return '';
+  const dnaArr = dna.split('');
+  let unknown = null;
+  const rnaArr = dnaArr.map((el) => {
+    if (conversionList[el] === undefined) unknown = true;
 
-        return conversionList[el];
+    return conversionList[el];
+  });
 
-    });
-
-    return unknown ? null : rnaArr.join('');
+  return unknown ? null : rnaArr.join('');
 };
