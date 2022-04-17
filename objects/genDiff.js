37f5ffe0529aa62d596b16249697ebfa3074145c
Rema04Dev <romansmerdyakov@gmail.com>
2022-04-17 17:17:35 +0300

    fix genDiff.js

--- objects/genDiff.js
+++ objects/genDiff.js
@@ -1,33 +1,24 @@
 import _ from 'lodash';
-// {
-//   one: 'deleted',
-//   two: 'changed',
-//   four: 'unchanged',
-//   zero: 'added',
-// }
-const genDiff = (object1, object2) => {
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
+
+const genDiff = (data1, data2) => {
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
   const keys = _.union(keys1, keys2);
+
   const result = {};
   for (const key of keys) {
-    if (object1[key] === object2[key]) {
+    if (!Object.hasOwn(data1, key)) {
+      result[key] = 'added';
+    } else if (!Object.hasOwn(data2, key)) {
+      result[key] = 'deleted';
+    } else if (data1[key] !== data2[key]) {
+      result[key] = 'changed';
+    } else {
       result[key] = 'unchanged';
     }
-    if (!_.has(object1, key)) {
-      result[key] = 'added';
-    }
-    if (!_.has(object2, key)) {
-      result[key] = 'deleted';
-    }
-    if (_.has(object1, key) && _.has(object2, key) && object1[key] !== object2[key]) {
-      result[key] = 'changed';
-    }
   }
+
   return result;
 };
-console.log(genDiff(
-  { one: 'eon', two: 'two', four: true },
-  { two: 'own', zero: 4, four: true },
-));
+
 export default genDiff;
