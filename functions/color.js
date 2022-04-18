efc64737cfc70222f22689219a8ad73e058035ec
Rema04Dev <romansmerdyakov@gmail.com>
2022-04-18 23:51:36 +0300

    fix color.js

--- functions/color.js
+++ functions/color.js
@@ -1,19 +1,5 @@
 import _ from 'lodash';
 
-// export const rgbToHex = (r, g, b) => {
-//   const hex = [r, g, b]
-//     .map((channel) => channel.toString(16).padStart(2, '0'))
-//     .join('');
-//   return `#${hex}`;
-// };
-
-// export const hexToRgb = (hex) => {
-//   const [r, g, b] = _.chunk(hex.slice(1), 2)
-//     .map((channel) => channel.join(''))
-//     .map((channel) => parseInt(channel, 16));
-//   return { r, g, b };
-// };
-
 export const hexToRgb = (hex) => {
   const hexCode = hex.slice(1);
   const [r, g, b] = _.chunk(hexCode, 2)
