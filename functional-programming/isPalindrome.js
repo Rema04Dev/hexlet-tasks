54790bf5643c63abd392e624b457c74cde0e6d6d
Rema04Dev <romansmerdyakov@gmail.com>
2022-05-01 16:45:59 +0300

    fix

--- functional-programming/isPalindrome.js
+++ functional-programming/isPalindrome.js
@@ -1,18 +1,3 @@
-// const isPalindrome = (str) => {
-//     if (str.length <= 1) {
-//         return true;
-//     }
-//     const firstLetter = str[0];
-//     const lastLetter = str[str.length - 1];
-//     if (firstLetter !== lastLetter) {
-//         return false;
-//     }
-//     const stringWithoutFirstAndLastLetter = str.substring(1, str.length - 1);
-//     return isPalindrome(stringWithoutFirstAndLastLetter);
-// }
-
-// export default isPalindrome;
-
 const isPalindrome = (str) => {
     if (str.length <= 1) {
         return true;
